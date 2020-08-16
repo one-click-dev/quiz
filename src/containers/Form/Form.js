4475c54b7908e85b58d55b8f3983afb1d51768a4
livakivska <livakivska@gmail.com>
2020-08-16 18:28:10 +0200

    Ignore warnings for email regex

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -18,6 +18,7 @@ export default function FormComponent() {
         [formError, setFormError] = useState(false),
         [formSubmitSuccess, setFormSubmitSuccess] = useState(false)
 
+    // eslint-disable-next-line
     const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     
     const submitHandler = (e) => {
